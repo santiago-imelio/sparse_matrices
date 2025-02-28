da2f01e8267feef43b8c4894290ad56ca2c9083e
Santiago Imelio <santiago.imelio@gmail.com>
2025-02-28 11:37:26 -0300

    adpat csr_demo to bcsr_demo data

--- csr/csr_demo.c
+++ csr/csr_demo.c
@@ -4,21 +4,22 @@
 
 int main()
 {
-    int nnz = 9;
-    int m = 4;
-    int n = 4;
+	int nnz = 12;
+	int m = 5;
+	int n = 5;
     float *values = (float *)malloc(nnz * sizeof(float));
     int *col_idx = (int *)malloc(nnz * sizeof(int));
     int *row_ptr = (int *)malloc((m + 1) * sizeof(int));
 
-    for (int i = 0; i < nnz; i++)
-        values[i] = i + 1;
+	float values_static[] = {5, 1, 7, 1, 2, 3, 2, 4, 1, 3, 6, 3};
+	for (int i = 0; i < nnz; i++)
+		values[i] = values_static[i];
 
-    int col_idx_static[] = {0, 1, 2, 3, 0, 1, 2, 3, 2};
+    int col_idx_static[] = {0, 1, 2, 1, 3, 4, 1, 2, 2, 3, 1, 4};
     for (int i = 0; i < nnz; i++)
         col_idx[i] = col_idx_static[i];
 
-    int row_ptr_static[] = {0, 4, 6, 8, 9};
+    int row_ptr_static[] = {0, 3, 6, 8, 10, 13};
     for (int i = 0; i <= m; i++)
         row_ptr[i] = row_ptr_static[i];
 
